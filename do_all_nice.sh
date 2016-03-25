68153d52c08341310702b57fb04d9a69cb91d701
Andrey Mikheev <solver.it@gmail.com>
2016-03-25 16:39:33 +0300

    Create do_all_nice.sh

---
+++ do_all_nice.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#Начальное обновление системы
+apt-get update
+apt-get -y --force-yes dist-upgrade
+
+#Добавляем нужные репы
+add-apt-repository ppa:graphics-drivers/ppa
+add-apt-repository ppa:git-core/ppa
+add-apt-repository ppa:webupd8team/java
+add-apt-repository ppa:libreoffice/ppa
+add-apt-repository ppa:peterlevi/ppa
+apt-add-repository ppa:shutter/ppa
+
+#Проверим ключи репозиториев
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com `apt-get update 2>&1 | grep -o '[0-9A-Z]\{16\}$' | xargs`
+
+#Обновление пакетов
+apt-get update
+
+#Фиксим кодировки
+gsettings set org.gnome.gedit.preferences.encodings auto-detected "['UTF-8', 'WINDOWS-1251', 'CURRENT', 'ISO-8859-15', 'UTF-16']"
+
+#Устанавливаем нужные пакеты
+PACKAGES="indicator-multiload 
+git maven oracle-java7-installer oracle-java8-installer 
+gtk-redshift gnome-system-tools unity-tweak-tool
+chromium-browser pepperflashplugin-nonfree 
+variety shutter p7zip vlc "
+
+sudo apt-get -y --force-yes install $PACKAGES
+
+#Свежий Flash
+update-pepperflashplugin-nonfree --install
+
+
+# sudo apt-get install nvidia-364 nvidia-settings
