985cc2afb5a48d00dff559bf236499a51f1e9cb2
Andrey Mikheev <solver.it@gmail.com>
2016-04-25 13:31:49 +0300

    Update do_all_nice.sh

--- do_all_nice.sh
+++ do_all_nice.sh
@@ -5,12 +5,12 @@ apt-get update
 apt-get -y --force-yes dist-upgrade
 
 #Добавляем нужные репы
-add-apt-repository ppa:graphics-drivers/ppa
-add-apt-repository ppa:git-core/ppa
-add-apt-repository ppa:webupd8team/java
-add-apt-repository ppa:libreoffice/ppa
-add-apt-repository ppa:peterlevi/ppa
-apt-add-repository ppa:shutter/ppa
+add-apt-repository --yes ppa:graphics-drivers/ppa
+add-apt-repository --yes ppa:git-core/ppa
+add-apt-repository --yes ppa:webupd8team/java
+add-apt-repository --yes ppa:libreoffice/ppa
+add-apt-repository --yes ppa:peterlevi/ppa
+apt-add-repository --yes ppa:shutter/ppa
 
 #Проверим ключи репозиториев
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com `apt-get update 2>&1 | grep -o '[0-9A-Z]\{16\}$' | xargs`
@@ -22,7 +22,7 @@ apt-get update
 gsettings set org.gnome.gedit.preferences.encodings auto-detected "['UTF-8', 'WINDOWS-1251', 'CURRENT', 'ISO-8859-15', 'UTF-16']"
 
 #Устанавливаем нужные пакеты
-PACKAGES="indicator-multiload 
+PACKAGES="gdebi indicator-multiload 
 git maven oracle-java7-installer oracle-java8-installer 
 gtk-redshift gnome-system-tools unity-tweak-tool
 chromium-browser pepperflashplugin-nonfree 
