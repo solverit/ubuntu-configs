8d9657aa22ad7d8a42a738761996b6471b16c212
Andrey Mikheev <solver.it@gmail.com>
2017-09-05 17:48:42 +0300

    add sbt
    add sbt

--- do_all_nice_kde.sh
+++ do_all_nice_kde.sh
@@ -13,19 +13,25 @@ add-apt-repository --yes ppa:libreoffice/ppa
 apt-add-repository --yes ppa:shutter/ppa
 
 #sublime
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
 
 #docker
 sudo apt-get -y --force-yes install curl
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
+   
+#sbt
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+echo "deb https://dl.bintray.com/sbt/debian /" | tee -a /etc/apt/sources.list.d/sbt.list
+
 
 #Проверим ключи репозиториев
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com `apt-get update 2>&1 | grep -o '[0-9A-Z]\{16\}$' | xargs`
+
 
 #Обновление пакетов
 apt-get update
@@ -33,7 +39,7 @@ apt-get -y --force-yes dist-upgrade
 
 
 #Устанавливаем нужные пакеты
-PACKAGES="docker-ce mc git maven redshift chromium-browser shutter p7zip vlc sublime-text nvidia-384 nvidia-settings"
+PACKAGES="docker-ce mc git maven sbt redshift chromium-browser shutter p7zip vlc sublime-text nvidia-384 nvidia-settings"
 
 sudo apt-get -y --force-yes install $PACKAGES
 
