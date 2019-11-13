d888ecca2337b883936c0052b76e23115ca66efd
Andrey Mikheev <solver.it@gmail.com>
2019-11-13 14:39:48 +0300

    Update do_all_nice.sh
    update config

--- do_all_nice.sh
+++ do_all_nice.sh
@@ -5,13 +5,13 @@ apt-get update
 apt-get -y --force-yes dist-upgrade
 
 #Добавляем нужные репы
-add-apt-repository --yes ppa:canonical-kernel-team/ppa
 add-apt-repository --yes ppa:graphics-drivers/ppa
 add-apt-repository --yes ppa:git-core/ppa
-add-apt-repository --yes ppa:webupd8team/java
 add-apt-repository --yes ppa:libreoffice/ppa
-add-apt-repository --yes ppa:peterlevi/ppa
-apt-add-repository --yes ppa:shutter/ppa
+
+#sbt
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+echo "deb https://dl.bintray.com/sbt/debian /" | tee -a /etc/apt/sources.list.d/sbt.list
 
 #Проверим ключи репозиториев
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com `apt-get update 2>&1 | grep -o '[0-9A-Z]\{16\}$' | xargs`
@@ -20,21 +20,16 @@ apt-key adv --recv-keys --keyserver keyserver.ubuntu.com `apt-get update 2>&1 | 
 apt-get update
 
 #Фиксим кодировки
-#gsettings set org.gnome.gedit.preferences.encodings auto-detected "['UTF-8', 'WINDOWS-1251', 'CURRENT', 'ISO-8859-15', 'UTF-16']"
 gsettings set org.gnome.gedit.preferences.encodings candidate-encodings "['UTF-8', 'WINDOWS-1251', 'KOI8-R', 'CURRENT', 'ISO-8859-15', 'UTF-16']"
 
 #Устанавливаем нужные пакеты
-PACKAGES="gdebi indicator-multiload 
-libreoffice libreoffice-l10n-ru libreoffice-help-ru
-git maven oracle-java7-installer oracle-java8-installer 
-chromium-browser pepperflashplugin-nonfree 
-variety shutter p7zip vlc 
-gtk-redshift gnome-system-tools unity-tweak-tool"
+PACKAGES="mc gdebi libreoffice libreoffice-l10n-ru libreoffice-help-ru
+git maven sbt chromium-browser p7zip vlc dconf-editor gnome-tweak-tool openvpn"
 
 sudo apt-get -y --force-yes install $PACKAGES
 
-#Свежий Flash
-update-pepperflashplugin-nonfree --install
+#env
+sudo usermod -aG docker $USER
+sudo usermod -aG vboxusers $USER
 
-
-# sudo apt-get install nvidia-364 nvidia-settings
+# sudo apt-get install nvidia-driver-435
